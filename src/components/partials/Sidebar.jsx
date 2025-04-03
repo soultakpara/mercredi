93ec1a2024606470c8d948ca4e7731cf0029bf43
Bernard <bernardfachola100@gmail.com>
2025-04-03 12:04:56 +0100

    [ADD] sidebar et form

--- src/components/partials/Sidebar.jsx
+++ src/components/partials/Sidebar.jsx
@@ -9,6 +9,8 @@ function Sidebar() {
             <li><a href="#Accueil">Accueil</a></li>
             <li><a href="#Profil">Profil</a></li>
             <li><a href="#Form">Form</a></li>
+            <li><a href="#FAQ">FAQ</a></li>
+            <li><a href="#Politiques et Confidentialité">Politiques et Confidentialité</a></li>
         </ul>
     </div>
   );
