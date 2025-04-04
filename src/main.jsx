2b149fbab993fc058398026d948ffeeaf79cd8d5
Bernard <bernardfachola100@gmail.com>
2025-04-04 11:24:17 +0100

    modification

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-<<<<<<< HEAD
-//import App from './App.jsx'
-//import Footer from './Footer.jsx'
-import Home from './Home.jsx'
+import Sidebar from './components/partials/Sidebar'
+import { Form } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home />
-=======
-import App from './App.jsx'
-import Sidebar from './components/partials/Sidebar.jsx'
-import Form from './components/form.jsx'
+  <StrictMode>  
+  {/* <App /> */}
+  {<Sidebar />}
+  {<Form />}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <App /> */}
-    {<Sidebar />}
-    {<Form />}
-
->>>>>>>) origin/SideBar
-=======
-// import App from './App.jsx'
-import Header from './components/partials/Header.jsx'
-import Accueil from './components/Accueil.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Accueil />
-    {/* {<Header />} */}
->>>>>>> origin/Header
-  </StrictMode>,
+<StrictMode />
 )
+  
