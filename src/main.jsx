951b6178fa52d964ab4261b5da75453adbc8a7aa
Bernard <bernardfachola100@gmail.com>
2025-04-04 11:27:34 +0100

    le meuilleur des codes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Sidebar from './components/partials/Sidebar'
-import { Form } from 'react-router-dom'
-
+import App from './App.jsx'
+import Sidebar from './components/partials/Sidebar.jsx'
+import Form from './components/form.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>  
-  {/* <App /> */}
-  {<Sidebar />}
-  {<Form />}
+  <StrictMode>
+    {/* <App /> */}
+    {<Sidebar />}
+    {<Form />}
 
-<StrictMode />
+  </StrictMode>,
 )
-  
